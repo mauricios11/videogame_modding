72830afbf0958e5e0344582c18769b0d0d8161f4
mauricios11 <mauricios00@protonmail.com>
2025-07-17 02:42:22 -0600

    update: lua physics file & Readme

--- Dragons_Dogma_2/physics/reference_code/reframework/autorun/physics_enhanced.lua
+++ Dragons_Dogma_2/physics/reference_code/reframework/autorun/physics_enhanced.lua
@@ -9,7 +9,7 @@ print("Initializing `physics_enhanced` v"..version)
 -- Converts "System.Enum" --> Lua (table) for easier access to enum values.
 local function generate_statics(typename)
 	local t = sdk.find_type_definition(typename)
-	local fields = t:get_fields()
+	local fields = t:get_fields() -- get all fields of the type definition
 	local enum = {}
 	local rev_enum = {}
 	local names = {}
@@ -20,23 +20,24 @@ local function generate_statics(typename)
 				local name = field:get_name()
 				enum[name] = raw_value
 				enum[raw_value] = name
-				table.insert(names, name)
+				table.insert(names, name) -- collect names for sorting
+				-- rev_enum[raw_value] = name -- reverse lookup
 			end
 		end
 	end
-	return enum, names
+	return enum, names 
 end
 
 -- Gets a dic from an RE (managed object).
-local function get_dict(dict, as_array, sort_fn)
+local function get_dict(dict, as_array, sort_fn) -- dict: the managed object, as_array: boolean, sort_fn: function
 	local output = {}
 	if not dict._entries then return output end
 	if as_array then
 		for i, value_obj in pairs(dict._entries) do
-			output[i] = value_obj.value
+			output[i] = value_obj.value -- value_obj.value is the actual value, key is the index in the array
 		end
 		if sort_fn then
-			table.sort(output, sort_fn)
+			table.sort(output, sort_fn) -- sort_fn should be a function that takes two arguments and returns true if the first argument should come before the second
 		end
 	else
 		for i, value_obj in pairs(dict._entries) do
@@ -50,13 +51,12 @@ end
 
 local chr_edit_mgr = sdk.get_managed_singleton("app.CharacterEditManager")
 
-local male = 2776536455
-local female = 1910070090
+local male = 2776536455   -- Male   character ID
+local female = 1910070090 -- Female character ID
 
 -- Female
 for enum_id, entry in pairs(get_dict(chr_edit_mgr._TopsDB[female])) do
     if entry._BreastSwaying > 0 then
-		entry._BreastSwaying = entry._BreastSwaying * 2
-        --entry._BreastSwaying = entry._BreastSwaying * 1000000 -- Not sure where the limit is, but this is high enough to go past it.
+		entry._BreastSwaying = entry._BreastSwaying * 2 --max --> 100000 
     end
 end
--- README.md
+++ README.md
@@ -8,10 +8,10 @@ Go to the link for the mod you want to download, then click the "download" butto
 ## 🔗 links:
 
 ### 🐉 Dragon's Dogma 2 Management Stamina Mod
-A file written in ´.Lua´ to change the amount of stamina usage. **This mod only works while you are NOT in combat**:
+A file written in `.Lua` to change the amount of stamina usage. **This mod only works while you are NOT in combat**:
 * WIP
 
 ### 🐉 Dragon's Dogma 2 Physics Mod
-A file written in ´.Lua´ to modify body physics:
+A file written in `.Lua` to modify body physics:
 * **[➡️ Download Mod v1.9 (RAR) ⬅️](https://github.com/mauricios11/videogame_modding/blob/main/Dragons_Dogma_2/physics/physics_enhanced.rar)**
   
